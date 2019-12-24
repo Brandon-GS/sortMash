b0f2c8b34d1a450534e2006d0f8dd508c5feadd4
ImGosuh <cosmoscrew1@gmail.com>
2019-12-23 19:59:35 -0600

    Updates

---
+++ precache-manifest.0ad94b6633aa27d7cec1cef524df5e7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3fe09ad5b351c6542059467ec3befaa",
+    "url": "/sortmash/index.html"
+  },
+  {
+    "revision": "e0faaf2d72093dbf29bb",
+    "url": "/sortmash/static/js/2.e7b23f0a.chunk.js"
+  },
+  {
+    "revision": "64be437c74fd47fbfe0ef57becb73f7b",
+    "url": "/sortmash/static/js/2.e7b23f0a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "25beb34441440d6c71f8",
+    "url": "/sortmash/static/js/main.7f6c29a3.chunk.js"
+  },
+  {
+    "revision": "3043bd8b5fefc989f1e2",
+    "url": "/sortmash/static/js/runtime-main.a3f057ba.js"
+  }
+]);
